81b98a7822cbcf81650409fbbadbdcbebb342df3
Valentin Varbanov <vvarbanov99@gmail.com>
2024-12-19 19:40:52 +0200

    Create temp file

--- src/main/java/com/greydev/notionbackup/NotionBackup.java
+++ src/main/java/com/greydev/notionbackup/NotionBackup.java
@@ -1,86 +1,76 @@
 package com.greydev.notionbackup;
 
-import java.io.File;
-import java.io.IOException;
-import java.rmi.UnexpectedException;
-import java.util.Optional;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.atomic.AtomicBoolean;
-
-import com.greydev.notionbackup.cloudstorage.pcloud.PCloudApiClientFactory;
-import com.greydev.notionbackup.cloudstorage.pcloud.PCloudClient;
-import com.pcloud.sdk.ApiClient;
-import com.pcloud.sdk.RemoteFolder;
+import com.google.api.services.drive.Drive;
+import com.greydev.notionbackup.cloudstorage.googledrive.GoogleDriveClient;
+import com.greydev.notionbackup.cloudstorage.googledrive.GoogleDriveServiceFactory;
+import io.github.cdimascio.dotenv.Dotenv;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.codec.CharEncoding;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 
-import com.dropbox.core.v2.DbxClientV2;
-import com.dropbox.core.DbxException;
-import com.dropbox.core.DbxRequestConfig;
-import com.dropbox.core.oauth.DbxCredential;
-import com.dropbox.core.oauth.DbxRefreshResult;
-import com.google.api.services.drive.Drive;
-import com.greydev.notionbackup.cloudstorage.dropbox.DropboxClient;
-import com.greydev.notionbackup.cloudstorage.dropbox.DropboxServiceFactory;
-import com.greydev.notionbackup.cloudstorage.googledrive.GoogleDriveClient;
-import com.greydev.notionbackup.cloudstorage.googledrive.GoogleDriveServiceFactory;
-import com.greydev.notionbackup.cloudstorage.nextcloud.NextcloudClient;
-
-import io.github.cdimascio.dotenv.Dotenv;
-import lombok.extern.slf4j.Slf4j;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 
 @Slf4j
 public class NotionBackup {
 
-	public static final String KEY_DROPBOX_ACCESS_TOKEN = "DROPBOX_ACCESS_TOKEN";
-	public static final String KEY_DROPBOX_APP_KEY = "DROPBOX_APP_KEY";
-	public static final String KEY_DROPBOX_APP_SECRET = "DROPBOX_APP_SECRET";
-	public static final String KEY_DROPBOX_REFRESH_TOKEN = "DROPBOX_REFRESH_TOKEN";
+    public static final String KEY_DROPBOX_ACCESS_TOKEN = "DROPBOX_ACCESS_TOKEN";
+    public static final String KEY_DROPBOX_APP_KEY = "DROPBOX_APP_KEY";
+    public static final String KEY_DROPBOX_APP_SECRET = "DROPBOX_APP_SECRET";
+    public static final String KEY_DROPBOX_REFRESH_TOKEN = "DROPBOX_REFRESH_TOKEN";
 
-	public static final String KEY_NEXTCLOUD_EMAIL = "NEXTCLOUD_EMAIL";
-	public static final String KEY_NEXTCLOUD_PASSWORD = "NEXTCLOUD_PASSWORD";
-	public static final String KEY_NEXTCLOUD_WEBDAV_URL = "NEXTCLOUD_WEBDAV_URL";
+    public static final String KEY_NEXTCLOUD_EMAIL = "NEXTCLOUD_EMAIL";
+    public static final String KEY_NEXTCLOUD_PASSWORD = "NEXTCLOUD_PASSWORD";
+    public static final String KEY_NEXTCLOUD_WEBDAV_URL = "NEXTCLOUD_WEBDAV_URL";
 
-	private static final String KEY_GOOGLE_DRIVE_ROOT_FOLDER_ID = "GOOGLE_DRIVE_ROOT_FOLDER_ID";
-	private static final String KEY_GOOGLE_DRIVE_SERVICE_ACCOUNT_SECRET_JSON = "GOOGLE_DRIVE_SERVICE_ACCOUNT_SECRET_JSON";
-	private static final String KEY_GOOGLE_DRIVE_SERVICE_ACCOUNT_SECRET_FILE_PATH = "GOOGLE_DRIVE_SERVICE_ACCOUNT_SECRET_FILE_PATH";
+    private static final String KEY_GOOGLE_DRIVE_ROOT_FOLDER_ID = "GOOGLE_DRIVE_ROOT_FOLDER_ID";
+    private static final String KEY_GOOGLE_DRIVE_SERVICE_ACCOUNT_SECRET_JSON = "GOOGLE_DRIVE_SERVICE_ACCOUNT_SECRET_JSON";
+    private static final String KEY_GOOGLE_DRIVE_SERVICE_ACCOUNT_SECRET_FILE_PATH = "GOOGLE_DRIVE_SERVICE_ACCOUNT_SECRET_FILE_PATH";
 
-	private static final String KEY_PCLOUD_ACCESS_TOKEN = "PCLOUD_ACCESS_TOKEN";
-	private static final String KEY_PCLOUD_API_HOST = "PCLOUD_API_HOST";
-	private static final String KEY_PCLOUD_FOLDER_ID = "PCLOUD_FOLDER_ID";
+    private static final String KEY_PCLOUD_ACCESS_TOKEN = "PCLOUD_ACCESS_TOKEN";
+    private static final String KEY_PCLOUD_API_HOST = "PCLOUD_API_HOST";
+    private static final String KEY_PCLOUD_FOLDER_ID = "PCLOUD_FOLDER_ID";
 
-	private static final Dotenv dotenv;
+    private static final Dotenv dotenv;
 
-	static {
-		dotenv = initDotenv();
-	}
+    static {
+        dotenv = initDotenv();
+    }
 
-	public static void main(String[] args) {
-		log.info("---------------- Starting Notion Backup ----------------");
+    public static void main(String[] args) throws IOException {
+        log.info("---------------- Starting Notion Backup ----------------");
 
-		NotionClient notionClient = new NotionClient(dotenv);
+        NotionClient notionClient = new NotionClient(dotenv);
 
 //		final File exportedFile = notionClient.export()
 //				.orElseThrow(() -> new IllegalStateException("Could not export notion file"));
 
-		final File exportedFile = new File(dotenv.get("DOWNLOADS_DIRECTORY_PATH"));
+        Files.write(Paths.get(dotenv.get("DOWNLOADS_DIRECTORY_PATH")), List.of("Line"), StandardCharsets.UTF_8);
+        final File exportedFile = new File(dotenv.get("DOWNLOADS_DIRECTORY_PATH"));
 
-		// use a local file to skip the notion export step
-		// final File exportedFile = new File("notion-export-markdown_2022-01-18_23-17-13.zip");
+        // use a local file to skip the notion export step
+        // final File exportedFile = new File("notion-export-markdown_2022-01-18_23-17-13.zip");
 
-		AtomicBoolean hasErrorOccurred = new AtomicBoolean(false);
+        AtomicBoolean hasErrorOccurred = new AtomicBoolean(false);
 
-		CompletableFuture
-				.runAsync(() -> NotionBackup.startGoogleDriveBackup(exportedFile))
-				.handle((result, ex) -> {
-					if (ex != null) {
-						log.error("Exception while GoogleDrive upload", ex);
-						hasErrorOccurred.set(true);
-					}
-					return null;
-				}).join();
+        CompletableFuture
+                .runAsync(() -> NotionBackup.startGoogleDriveBackup(exportedFile))
+                .handle((result, ex) -> {
+                    if (ex != null) {
+                        log.error("Exception while GoogleDrive upload", ex);
+                        hasErrorOccurred.set(true);
+                    }
+                    return null;
+                }).join();
 
 //		CompletableFuture<Void> futureDropbox = CompletableFuture
 //				.runAsync(() -> NotionBackup.startDropboxBackup(exportedFile))
@@ -114,35 +104,35 @@ public class NotionBackup {
 
 //		CompletableFuture.allOf(futureGoogleDrive, futureDropbox, futureNextcloud, futurePCloud).join();
 
-		if (hasErrorOccurred.get()) {
-			log.error("Not all backups were completed successfully. See the logs above to get more information about the errors.");
-			System.exit(1);
-		}
-	}
+        if (hasErrorOccurred.get()) {
+            log.error("Not all backups were completed successfully. See the logs above to get more information about the errors.");
+            System.exit(1);
+        }
+    }
 
-	public static void startGoogleDriveBackup(File fileToUpload) {
-		Optional<String> serviceAccountSecretOptional = extractGoogleServiceAccountSecret();
-		if (serviceAccountSecretOptional.isEmpty()) {
-			log.info("Skipping Google Drive upload. No secret provided for Google Drive.");
-			return;
-		}
-		Optional<Drive> googleServiceOptional = GoogleDriveServiceFactory.create(serviceAccountSecretOptional.get());
-		if (googleServiceOptional.isEmpty()) {
-			log.warn("Skipping Google Drive upload. Could not create Google Drive service.");
-			return;
-		}
-		String googleDriveRootFolderId = dotenv.get(KEY_GOOGLE_DRIVE_ROOT_FOLDER_ID);
-		if (StringUtils.isBlank(googleDriveRootFolderId)) {
-			log.info("Skipping Google Drive upload. {} is blank.", KEY_GOOGLE_DRIVE_ROOT_FOLDER_ID);
-			return;
-		}
-		GoogleDriveClient GoogleDriveClient = new GoogleDriveClient(googleServiceOptional.get(), googleDriveRootFolderId);
-		boolean isSuccess = GoogleDriveClient.upload(fileToUpload);
+    public static void startGoogleDriveBackup(File fileToUpload) {
+        Optional<String> serviceAccountSecretOptional = extractGoogleServiceAccountSecret();
+        if (serviceAccountSecretOptional.isEmpty()) {
+            log.info("Skipping Google Drive upload. No secret provided for Google Drive.");
+            return;
+        }
+        Optional<Drive> googleServiceOptional = GoogleDriveServiceFactory.create(serviceAccountSecretOptional.get());
+        if (googleServiceOptional.isEmpty()) {
+            log.warn("Skipping Google Drive upload. Could not create Google Drive service.");
+            return;
+        }
+        String googleDriveRootFolderId = dotenv.get(KEY_GOOGLE_DRIVE_ROOT_FOLDER_ID);
+        if (StringUtils.isBlank(googleDriveRootFolderId)) {
+            log.info("Skipping Google Drive upload. {} is blank.", KEY_GOOGLE_DRIVE_ROOT_FOLDER_ID);
+            return;
+        }
+        GoogleDriveClient GoogleDriveClient = new GoogleDriveClient(googleServiceOptional.get(), googleDriveRootFolderId);
+        boolean isSuccess = GoogleDriveClient.upload(fileToUpload);
 
-		if (!isSuccess) {
-			throw new IllegalStateException("Backup was not successful");
-		}
-	}
+        if (!isSuccess) {
+            throw new IllegalStateException("Backup was not successful");
+        }
+    }
 
 //	public static void startDropboxBackup(File fileToUpload) {
 //		Optional<String> dropboxAccessToken = getDropboxAccessToken();
@@ -245,45 +235,45 @@ public class NotionBackup {
 //		}
 //	}
 
-	private static Optional<String> extractGoogleServiceAccountSecret() {
-		String serviceAccountSecret = dotenv.get(KEY_GOOGLE_DRIVE_SERVICE_ACCOUNT_SECRET_JSON);
-		String serviceAccountSecretFilePath = dotenv.get(KEY_GOOGLE_DRIVE_SERVICE_ACCOUNT_SECRET_FILE_PATH);
+    private static Optional<String> extractGoogleServiceAccountSecret() {
+        String serviceAccountSecret = dotenv.get(KEY_GOOGLE_DRIVE_SERVICE_ACCOUNT_SECRET_JSON);
+        String serviceAccountSecretFilePath = dotenv.get(KEY_GOOGLE_DRIVE_SERVICE_ACCOUNT_SECRET_FILE_PATH);
 
-		// Use the secret value if provided.
-		if (StringUtils.isNotBlank(serviceAccountSecret)) {
-			return Optional.of(serviceAccountSecret);
-		}
-		// If not then read the value from the given path to the file which contains the secret
-		if (StringUtils.isNotBlank(serviceAccountSecretFilePath)) {
-			return readFileContentAsString(serviceAccountSecretFilePath);
-		}
-		log.info("Both {} and {} are empty", KEY_GOOGLE_DRIVE_SERVICE_ACCOUNT_SECRET_JSON, KEY_GOOGLE_DRIVE_SERVICE_ACCOUNT_SECRET_FILE_PATH);
-		return Optional.empty();
-	}
+        // Use the secret value if provided.
+        if (StringUtils.isNotBlank(serviceAccountSecret)) {
+            return Optional.of(serviceAccountSecret);
+        }
+        // If not then read the value from the given path to the file which contains the secret
+        if (StringUtils.isNotBlank(serviceAccountSecretFilePath)) {
+            return readFileContentAsString(serviceAccountSecretFilePath);
+        }
+        log.info("Both {} and {} are empty", KEY_GOOGLE_DRIVE_SERVICE_ACCOUNT_SECRET_JSON, KEY_GOOGLE_DRIVE_SERVICE_ACCOUNT_SECRET_FILE_PATH);
+        return Optional.empty();
+    }
 
-	private static Optional<String> readFileContentAsString(String filePath) {
-		String fileContent = null;
-		try {
-			fileContent = FileUtils.readFileToString(new File(filePath), CharEncoding.UTF_8);
-		} catch (IOException e) {
-			log.warn("IOException while reading file in path: '{}'", filePath, e);
-			return Optional.empty();
-		}
-		if (StringUtils.isBlank(fileContent)) {
-			log.warn("File '{}' is empty", filePath);
-			return Optional.empty();
-		}
-		return Optional.of(fileContent);
-	}
+    private static Optional<String> readFileContentAsString(String filePath) {
+        String fileContent = null;
+        try {
+            fileContent = FileUtils.readFileToString(new File(filePath), CharEncoding.UTF_8);
+        } catch (IOException e) {
+            log.warn("IOException while reading file in path: '{}'", filePath, e);
+            return Optional.empty();
+        }
+        if (StringUtils.isBlank(fileContent)) {
+            log.warn("File '{}' is empty", filePath);
+            return Optional.empty();
+        }
+        return Optional.of(fileContent);
+    }
 
-	private static Dotenv initDotenv() {
-		Dotenv dotenv = Dotenv.configure()
-				.ignoreIfMissing()
-				.ignoreIfMalformed()
-				.load();
-		if (dotenv == null) {
-			throw new IllegalStateException("Could not load dotenv!");
-		}
-		return dotenv;
-	}
+    private static Dotenv initDotenv() {
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMissing()
+                .ignoreIfMalformed()
+                .load();
+        if (dotenv == null) {
+            throw new IllegalStateException("Could not load dotenv!");
+        }
+        return dotenv;
+    }
 }
